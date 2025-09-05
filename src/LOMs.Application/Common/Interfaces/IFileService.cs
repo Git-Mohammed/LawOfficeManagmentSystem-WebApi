b90a5ca6c7ee2049b031186684ae98df3ca0d6ae
Redaessa7 <redaesssa@gmail.com>
2025-09-05 17:45:16 +0300

    feat(file service): add file service and configuration

---
+++ src/LOMs.Application/Common/Interfaces/IFileService.cs
@@ -0,0 +1,9 @@
+﻿namespace LOMs.Application.Common.Interfaces;
+
+public interface IFileService
+{
+    Task<string> SaveFileAsync(Stream fileStream, string fileName, string contentType);
+    Task<Stream?> GetFileAsync(string filePath);
+    Task DeleteFileAsync(string filePath);
+    bool ValidateFile(string fileName, string contentType, long fileSize);
+}
